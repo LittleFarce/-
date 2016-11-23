2ba4988259db064949a557f07746a08db536ab40
LZX <LZX@neu6.edu.cn>
2016-11-23 23:27:58 +0800

    Initial Commit

---
+++ 贪吃蛇/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  贪吃蛇
+//
+//  Created by LZX on 2016/11/23.
+//  Copyright © 2016年 123. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
